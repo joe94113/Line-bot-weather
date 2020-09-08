714e743aca0b8fff73d7c37852e7ef393f2efe38
王冠智 <66311497+joe94113@users.noreply.github.com>
2020-09-08 16:26:11 +0800

    my topictour

---
+++ app_pixabay.py
@@ -0,0 +1,142 @@
+﻿from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+
+import configparser
+
+import requests
+import json
+
+import urllib
+import re
+import random
+
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+
+# 接收 LINE 的資訊
+
+# 請 pixabay 幫我們找圖
+def MakeAQI(station):
+    end_point = "http://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000259?filters=SiteName eq '" + \
+        station + "'&sort=SiteName&offset=0&limit=1000"
+
+    data = requests.get(end_point)
+    AQImsg = ""
+
+    if data.status_code == 500:
+        return "無 AQI 資料"
+    else:
+        AQIdata = data.json()["result"]["records"][0]
+        AQImsg += "AQI = " + AQIdata["AQI"] + "\n"
+        AQImsg += "PM2.5 = " + AQIdata["PM2.5"] + " μg/m3\n"
+        AQImsg += "PM10 = " + AQIdata["PM10"] + " μg/m3\n"
+        AQImsg += "空品：" + AQIdata["Status"]
+        return AQImsg
+
+
+def GetWeather(station):
+    end_point = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=rdec-key-123-45678-011121314"
+
+    data = requests.get(end_point).json()
+    data = data["records"]["location"]
+
+    target_station = "not found"
+    for item in data:
+        if item["locationName"] == str(station):
+            target_station = item
+    return target_station
+
+
+def MakeWeather(station):
+    WeatherData = GetWeather(station)
+    if WeatherData == "not found":
+        return False
+
+    WeatherData = WeatherData["weatherElement"]
+    msg = "美濃趣天氣報告 - " + station
+    msg += "\n\n氣溫 = " + WeatherData[3]["elementValue"] + "℃\n"
+    msg += "濕度 = " + \
+        str(float(WeatherData[4]["elementValue"]) * 100) + "% RH\n"
+
+    msg += MakeAQI(station)
+    return msg
+
+
+def MakeRailFall(station):
+    result = requests.get(
+        "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=rdec-key-123-45678-011121314")
+    msg = "美濃趣降雨報告 - " + station + "\n\n"
+
+    if(result.status_code != 200):
+        return "雨量資料讀取失敗"
+    else:
+        railFallData = result.json()
+        for item in railFallData["records"]["location"]:
+            if station in item["locationName"]:
+                msg += "目前雨量：" + \
+                    item["weatherElement"][7]["elementValue"] + "mm\n"
+                if item["weatherElement"][3]["elementValue"] == "-998.00":
+                    msg += "三小時雨量：0.00mm\n"
+                else:
+                    msg += "三小時雨量：" + \
+                        item["weatherElement"][3]["elementValue"] + "mm\n"
+                msg += "日雨量：" + \
+                    item["weatherElement"][6]["elementValue"] + "mm\n"
+                return msg
+        return "沒有這個測站啦"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    cmd = event.message.text.split(" ")
+
+    if cmd[0] == "天氣":
+        station = cmd[1]
+        WeatherMsg = MakeWeather(station)
+
+        if not WeatherMsg:
+            WeatherMsg = "沒這個氣象站啦"
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=WeatherMsg))
+    if cmd[0] == "雨量":
+        station = cmd[1]
+        RailFallMsg = MakeRailFall(station)
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=RailFallMsg))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
